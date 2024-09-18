302b0c00584dc4127ba1ef8ce9f6a85a32bb4665
Nick Aguilar <nickaguilarh@gmail.com>
2024-09-17 23:20:43 -0500

    refactor: update telemetry error variable names for clarity
    Rename telemetry error variables to improve clarity and consistency. This change enhances the readability of the code by using more descriptive names. The changes affect the error handling in the telemetry package for metrics, logs, and traces.
    
    Breaking change: Renamed ErrTelemetryMetricsNotEnabled to ErrTelemetryMeterNotEnabled, ErrTelemetryLogsNotEnabled to ErrTelemetryLoggerNotEnabled, ErrTelemetryTracesNotEnabled to ErrTelemetryTracerNotEnabled.
    
    Signed-off-by: Nick Aguilar <nickaguilarh@gmail.com>

--- pkg/errors/error.go
+++ pkg/errors/error.go
@@ -4,6 +4,6 @@ import "errors"
 
 var ErrTelemetryNotEnabled = errors.New("telemetry is not enabled")
 
-var ErrTelemetryMetricsNotEnabled = errors.New("telemetry metrics is not enabled")
-var ErrTelemetryLogsNotEnabled = errors.New("telemetry logs is not enabled")
-var ErrTelemetryTracesNotEnabled = errors.New("telemetry traces is not enabled")
+var ErrTelemetryMeterNotEnabled = errors.New("telemetry metrics is not enabled")
+var ErrTelemetryLoggerNotEnabled = errors.New("telemetry logs is not enabled")
+var ErrTelemetryTracerNotEnabled = errors.New("telemetry traces is not enabled")
